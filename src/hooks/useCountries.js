ea9dc9e85364bb88b9d6a084ea1eca65390c7d35
abdalla shaker <167374151+abdalla-shaker@users.noreply.github.com>
2026-08-08 00:08:22 +0300

    Change countries API endpoint to v3.1
    Updated API endpoint for fetching countries data.

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -11,8 +11,8 @@ const useCountries = () => {
 
       try {
         const response = await fetch(
-          'https://api.restcountries.com/countries/v5?pretty=1',
-          { headers: { 'Authorization': 'Bearer rc_live_548071cc9004404db0d3b080aba8ea6e' } }
+          'https://restcountries.com/v3.1/all'
+          
         );
         if (!response.ok) {
           throw new Error("Failed to fetch countries data");
